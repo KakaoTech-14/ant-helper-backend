309deb02084cdc8037531d2fdb9c5649c3de53f8
hl3470 <hl3470@naver.com>
2024-08-13 17:15:49 +0900

    feature: Encoder 구현
    password를 인코딩하기 위한 Encoder 서비스를 구현하였다.

---
+++ backend/src/main/java/kakaobootcamp/backend/common/util/encoder/PasswordEncoderService.java
@@ -0,0 +1,23 @@
+package kakaobootcamp.backend.common.util.encoder;
+
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class PasswordEncoderService {
+
+	private final PasswordEncoder passwordEncoder;
+
+	// 패스워드 인코딩
+	private String encodePassword(String rawPw) {
+		return passwordEncoder.encode(rawPw);
+	}
+
+	// 요청된 pw와 서버의 pw가 일치하는 지 확인
+	public boolean matchPassword(String rawPw, String pw) {
+		return passwordEncoder.matches(rawPw, pw);
+	}
+}
