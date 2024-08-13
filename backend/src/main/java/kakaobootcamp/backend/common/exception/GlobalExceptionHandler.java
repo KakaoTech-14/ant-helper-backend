a22c1a7ff2b9e9ba34b117c0afdb236a9b321413
hl3470 <hl3470@naver.com>
2024-08-13 16:15:59 +0900

    feature: GlobalExceptionHandler 구현
    전역에서 발생하는 예외를 처리해주는 예외를 처리해주는 GlobalExceptionHandler를 구현하였다.

---
+++ backend/src/main/java/kakaobootcamp/backend/common/exception/GlobalExceptionHandler.java
@@ -0,0 +1,96 @@
+package kakaobootcamp.backend.common.exception;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatusCode;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.context.request.WebRequest;
+import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import org.springframework.web.servlet.resource.NoResourceFoundException;
+
+import kakaobootcamp.backend.common.dto.ErrorResponse;
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@RestControllerAdvice
+public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
+
+	@ExceptionHandler(CustomException.class)
+	public ResponseEntity<Object> handleCustomException(CustomException e) {
+		log.warn("handleCustomException", e);
+
+		return makeErrorResponseEntity(e.getErrorCode());
+	}
+
+	@Override
+	protected ResponseEntity<Object> handleNoResourceFoundException(
+		NoResourceFoundException ex,
+		HttpHeaders headers,
+		HttpStatusCode status,
+		WebRequest request) {
+
+		log.warn("handleNoResourceFoundException", ex);
+
+		ErrorCode errorCode = ErrorCode.RESOURCE_NOT_FOUND;
+
+		return makeErrorResponseEntity(errorCode);
+	}
+
+	@Override
+	public ResponseEntity<Object> handleMethodArgumentNotValid(
+		MethodArgumentNotValidException e,
+		HttpHeaders headers,
+		HttpStatusCode status,
+		WebRequest request) {
+
+		log.warn("handleIllegalArgument", e);
+
+		List<String> messages = e.getBindingResult().getFieldErrors()
+			.stream()
+			.map(ex -> ex.getDefaultMessage())
+			.collect(Collectors.toList());
+
+		ErrorCode errorCode = ErrorCode.INVALID_PARAMETER;
+		return makeErrorResponseEntity(errorCode, messages);
+	}
+
+	@Override
+	protected ResponseEntity<Object> handleHttpMessageNotReadable(
+		HttpMessageNotReadableException ex,
+		HttpHeaders headers,
+		HttpStatusCode status,
+		WebRequest request) {
+
+		log.warn("handleHttpMessageNotReadableException", ex);
+
+		ErrorCode errorCode = ErrorCode.BAD_REQUEST;
+		return makeErrorResponseEntity(errorCode);
+	}
+
+	@ExceptionHandler({Exception.class})
+	public ResponseEntity<Object> handleAllException(Exception e) {
+		log.warn("handleAllException", e);
+
+		ErrorCode errorCode = ErrorCode.INTERNAL_SERVER_ERROR;
+		return makeErrorResponseEntity(errorCode);
+	}
+
+	// ErrorCode를 받아서 Response를 만드는 메서드
+	private ResponseEntity<Object> makeErrorResponseEntity(ErrorCode errorCode) {
+		return ResponseEntity
+			.status(errorCode.getHttpStatus())
+			.body(ErrorResponse.from(errorCode));
+	}
+
+	private ResponseEntity<Object> makeErrorResponseEntity(ErrorCode errorCode, List<String> message) {
+		return ResponseEntity
+			.status(errorCode.getHttpStatus())
+			.body(ErrorResponse.of(errorCode, message));
+	}
+}
