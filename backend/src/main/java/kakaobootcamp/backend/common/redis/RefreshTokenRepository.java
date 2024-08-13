b60c9dc2661736ef1526625c1f3bfcca2437d92f
hl3470 <hl3470@naver.com>
2024-08-13 18:36:22 +0900

    feature: RefreshToken 구현

---
+++ backend/src/main/java/kakaobootcamp/backend/common/redis/RefreshTokenRepository.java
@@ -0,0 +1,14 @@
+package kakaobootcamp.backend.common.redis;
+
+import java.util.Optional;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RefreshTokenRepository extends CrudRepository<RefreshToken, String> {
+
+	Optional<RefreshToken> findByMemberId(Long memberId);
+
+	void deleteByMemberId(Long MemberId);
+}
