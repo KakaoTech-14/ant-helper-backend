ded65230b7c19bf13c8e28d96c3fb953e6f56ded
hl3470 <hl3470@naver.com>
2024-08-15 15:32:21 +0900

    feature: JwtAuthenticationFilter 구현

--- backend/src/main/java/kakaobootcamp/backend/common/exception/ErrorCode.java
+++ backend/src/main/java/kakaobootcamp/backend/common/exception/ErrorCode.java
@@ -7,18 +7,23 @@ import lombok.Getter;
 @Getter
 public enum ErrorCode {
 
-	//4xx
+	//400
 	BAD_REQUEST(HttpStatus.BAD_REQUEST, "잘못된 요청입니다."),
+	INVALID_PARAMETER(HttpStatus.BAD_REQUEST, "요청 파라미터가 잘 못 되었습니다."),
+
+	//401
+	UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "인증이 필요합니다."),
 	INVALID_ACCESS_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 액세스 토큰입니다."),
 	INVALID_REFRESH_TOKEN(HttpStatus.UNAUTHORIZED, "유효하지 않은 리프레시 토큰입니다."),
 
+	//402
+	REISSUE_ACCESS_TOKEN(HttpStatus.PAYMENT_REQUIRED, "액세스 토큰 재발급이 필요합니다."),
+
+	//403
 	MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "해당하는 회원을 찾을 수 없습니다."),
-
-	INVALID_PARAMETER(HttpStatus.BAD_REQUEST, "요청 파라미터가 잘 못 되었습니다."),
-
 	RESOURCE_NOT_FOUND(HttpStatus.NOT_FOUND, "해당 리소스를 찾을 수 없습니다."),
 
-	//5xx
+	//500
 	INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버 내부에서 에러가 발생하였습니다.");
 
 	private final HttpStatus httpStatus;
