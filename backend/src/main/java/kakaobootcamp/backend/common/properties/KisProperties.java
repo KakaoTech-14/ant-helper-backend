34e81c74d590dcd933e3eb045013b2e54c303460
hl3470 <hl3470@naver.com>
2024-08-20 01:20:46 +0900

    feature: KisProperties 구현

---
+++ backend/src/main/java/kakaobootcamp/backend/common/properties/KisProperties.java
@@ -0,0 +1,12 @@
+package kakaobootcamp.backend.common.properties;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@ConfigurationProperties(prefix = "kis")
+public class KisProperties {
+}
