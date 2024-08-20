b05ea94b8e193cbd2529cec2353bf1a6594b03d4
hl3470 <hl3470@naver.com>
2024-08-20 14:52:32 +0900

    refactor: 테스트db 분리

--- backend/src/test/java/kakaobootcamp/backend/domains/member/MemberControllerTest.java
+++ backend/src/test/java/kakaobootcamp/backend/domains/member/MemberControllerTest.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
@@ -26,6 +27,7 @@ import kakaobootcamp.backend.domains.member.dto.MemberDTO.CreateMemberRequest;
 @DisplayName("MemberController 테스트")
 @SpringBootTest
 @AutoConfigureMockMvc
+@ActiveProfiles({"test", "common"})
 class MemberControllerTest {
 
 	@Autowired
