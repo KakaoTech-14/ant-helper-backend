eeb744b66bd29e1e67d4977b0ae8866b537207f5
hl3470 <hl3470@naver.com>
2024-08-30 15:22:20 +0900

    refactor: AiService의 getOrderList 메서드 Member애서 가져오도록 변경
    AiService의 getOrderList 메서드 Member애서 가져오도록 변경 및 변경에 따라 Controller의 불필요한 메서드를 삭제했다.

--- backend/src/main/java/kakaobootcamp/backend/domains/aiServer/AiServerController.java
+++ backend/src/main/java/kakaobootcamp/backend/domains/aiServer/AiServerController.java
@@ -1,15 +1,11 @@
 package kakaobootcamp.backend.domains.aiServer;
 
-import java.util.List;
-
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import kakaobootcamp.backend.common.dto.DataResponse;
-import kakaobootcamp.backend.domains.aiServer.dto.AiServerDTO;
-import kakaobootcamp.backend.domains.aiServer.dto.AiServerDTO.GetOrderListRequest.StockOrderDTO;
 import lombok.RequiredArgsConstructor;
 
 @RestController
@@ -18,21 +14,4 @@ import lombok.RequiredArgsConstructor;
 public class AiServerController {
 
 	private final AiServerService aiServerService;
-
-	@GetMapping
-	public ResponseEntity<DataResponse<Void>> get() {
-
-		// 이 부분 추후 변경
-		List<StockOrderDTO> stocks = List.of(
-			new StockOrderDTO("005930", "삼성전자"),
-			new StockOrderDTO("066570", "LG전자")
-		);
-
-		AiServerDTO.GetOrderListRequest request = new AiServerDTO.GetOrderListRequest(stocks);
-
-		aiServerService.getOrderList(request);
-
-		return ResponseEntity.ok(DataResponse.ok());
-	}
-
 }
--- backend/src/main/java/kakaobootcamp/backend/domains/aiServer/AiServerService.java
+++ backend/src/main/java/kakaobootcamp/backend/domains/aiServer/AiServerService.java
@@ -5,8 +5,10 @@ import java.util.HashMap;
 import org.springframework.stereotype.Service;
 
 import kakaobootcamp.backend.common.util.webClient.WebClientUtil;
-import kakaobootcamp.backend.domains.aiServer.dto.AiServerDTO.GetOrderListRequest;
 import kakaobootcamp.backend.domains.aiServer.dto.AiServerDTO.GetOrderListResponse;
+import kakaobootcamp.backend.domains.member.domain.Member;
+import kakaobootcamp.backend.domains.transaction.TransactionService;
+import kakaobootcamp.backend.domains.transaction.dto.TransactionDTO.GetTransactionDTO;
 import lombok.RequiredArgsConstructor;
 
 @Service
@@ -14,9 +16,12 @@ import lombok.RequiredArgsConstructor;
 public class AiServerService {
 
 	private final WebClientUtil webClientUtil;
+	private final TransactionService transactionService;
 
-	public GetOrderListResponse getOrderList(GetOrderListRequest request) {
+	public GetOrderListResponse getOrderList(Member member) {
 		String url = "/api/stocks/evaluation";
+
+		GetTransactionDTO request = transactionService.getTransaction(member);
 
 		return webClientUtil.postFromAiServer(
 			new HashMap<>(),
