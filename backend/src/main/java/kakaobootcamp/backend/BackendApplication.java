7e3b264a7b4814f1feafa37b6857e476201471ef
hl3470 <hl3470@naver.com>
2024-09-03 17:09:21 +0900

    fix: swagger 에러 해결

--- backend/src/main/java/kakaobootcamp/backend/BackendApplication.java
+++ backend/src/main/java/kakaobootcamp/backend/BackendApplication.java
@@ -4,8 +4,12 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.servers.Server;
+
 @EnableJpaAuditing
 @SpringBootApplication
+@OpenAPIDefinition(servers = {@Server(url = "/", description = "https://www.api.ant-helper.com")})
 public class BackendApplication {
 
 	public static void main(String[] args) {
