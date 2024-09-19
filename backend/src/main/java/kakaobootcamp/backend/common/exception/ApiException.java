8c082e509ae137e0a16caaa0dbe76eb131061661
hl3470 <hl3470@naver.com>
2024-09-19 16:39:43 +0900

    refactor: CustomException을 ApiException에 통일

--- backend/src/main/java/kakaobootcamp/backend/common/exception/ApiException.java
+++ backend/src/main/java/kakaobootcamp/backend/common/exception/ApiException.java
@@ -1,16 +1,24 @@
 package kakaobootcamp.backend.common.exception;
 
-import lombok.AccessLevel;
-import lombok.AllArgsConstructor;
+import org.springframework.http.HttpStatus;
+
 import lombok.Getter;
 
 @Getter
-@AllArgsConstructor(access = AccessLevel.PRIVATE)
 public class ApiException extends RuntimeException {
 
-	private final ErrorCode errorCode;
+	private final HttpStatus httpStatus;
+
+	public ApiException(HttpStatus httpStatus, String message) {
+		super(message);
+		this.httpStatus = httpStatus;
+	}
 
 	public static ApiException from(ErrorCode errorCode) {
-		return new ApiException(errorCode);
+		return new ApiException(errorCode.getHttpStatus(), errorCode.getMessage());
+	}
+
+	public static ApiException of(HttpStatus httpStatus, String message) {
+		return new ApiException(httpStatus, message);
 	}
 }
